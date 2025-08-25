9b7206f84131d3c446d3e0afbf1097cc48afc2d2
gmanka <gmankab@gmail.com>
2025-08-25 22:35:43 +0500

    [+] faithful

--- convert.sh
+++ convert.sh
@@ -2,12 +2,13 @@
 
 set -uexo pipefail
 
-mkdir -p input/Default_1.21.8/assets/minecraft
+which c2c
+which 7z
+
+mkdir -p input/source/assets/minecraft
 mkdir output
-curl -L 'https://piston-data.mojang.com/v1/objects/a19d9badbea944a4369fd0059e53bf7286597576/client.jar' -o minecraft.jar
-7z x minecraft.jar 'assets/minecraft/textures/*'
-mv assets/minecraft/textures input/Default_1.21.8/assets/minecraft/
-curl -L https://codeberg.org/ostech/craft_to_clonia_textures/releases/download/4aug25/CTCloniaTextures-linux-amd64-04Aug25 -o c2c
-chmod +x c2c
-./c2c
-7z a minecraft-textures.7z output/default_1.21.8_mc_to_clonia/*
+curl -L $url -o source.zip
+7z x source.zip 'assets/minecraft/textures/*'
+mv assets/minecraft/textures input/source/assets/minecraft/
+c2c
+7z a $name.7z output/source_mc_to_clonia
